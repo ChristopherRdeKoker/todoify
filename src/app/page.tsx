f67e2657ca456101ba6039ad92c056d2d1584c7f
ChristopherRdeKoker <christopherdekoker@gmail.com>
2024-08-18 18:38:15 +0200

    some boilerplate for navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
 export default function Home() {
-  return <main>Im here yippie</main>;
+  return (
+    <main className=" bg-green-500 w-full">
+      <div className="h-[calc(100vh_-_5rem)] rounded-md justify-center flex items-center flex-col grow">
+        <form>
+          <h1>Login Page</h1>
+        </form>
+      </div>
+    </main>
+  );
 }
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,10 @@
+export function Navbar() {
+  return (
+    <div
+      className="h-[3rem] border-[0.1rem] border-slate-400 text-slate-500 hover:border-slate-600 active:bg-slate-500
+      active:text-white select-none px-[3rem] cursor-pointer bo flex flex-row justify-between items-center p-4"
+    >
+      <div>Welcome</div>
+    </div>
+  );
+}
