18be6fa0c6b23a160504fc5fa5ad8eca0fdcc5d9
ChristopherRdeKoker <christopherdekoker@gmail.com>
2024-08-24 12:38:10 +0200

    added twMerge

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 export function Navbar() {
   return (
-    <div className="h-[4rem] bg-white border-[0.1rem] border-slate-400 text-slate-500 hover:border-slate-800 select-none px-[3rem] cursor-pointer bo flex flex-row justify-between items-center p-4">
+    <div className="h-[4rem] bg-slate-300 border-[0.1rem] border-slate-400 text-slate-500 hover:border-slate-800 select-none px-[3rem] cursor-pointer bo flex flex-row justify-between items-center p-4">
       <div>Welcome</div>
     </div>
   );
--- src/components/Paperback.tsx
+++ src/components/Paperback.tsx
@@ -1,7 +1,21 @@
-export function Paperback({ children }: { children: React.ReactNode }) {
+import { HTMLAttributes } from "react";
+import { twMerge } from "tailwind-merge";
+
+export function Paperback({
+  children,
+  className = "",
+}: {
+  children: React.ReactNode;
+  className?: HTMLAttributes<HTMLDivElement>["className"];
+}) {
   if (!children) return <h1>problem with layout...</h1>;
   return (
-    <div className="h-full shadow-md shadow-slate-600 w-full flex flex-col gap-4 border-[0.1rem] border-slate-400 bg-slate-50 text-bold p-4 rounded-md">
+    <div
+      className={twMerge(
+        "h-full shadow-md shadow-slate-600 w-full flex flex-col gap-4 border-[0.1rem] border-slate-400 bg-slate-50 text-bold p-4 rounded-md",
+        className
+      )}
+    >
       {children}
     </div>
   );
