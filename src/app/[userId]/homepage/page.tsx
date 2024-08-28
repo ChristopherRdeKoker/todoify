c4123f1f37ec872283480109c76a8223af6db23d
ChristopherRdeKoker <christopherdekoker@gmail.com>
2024-08-28 21:36:58 +0200

    asd

--- src/app/[userId]/homepage/page.tsx
+++ src/app/[userId]/homepage/page.tsx
@@ -10,13 +10,19 @@ export default async function Homepage({ params }: { params: HomepageParams }) {
   if (getUser?.data?.error) return <div>{getUser?.data?.error ?? "there was an error"}</div>;
   if (!getUser?.data?.userQuery) return <p>fetching...</p>;
   return (
-    <div>
+    <div className="flex justify-center flex-grow">
+      <p>Daily message here</p>
+      <p>Weather</p>
+      <p>Stats</p>
+      <p>.....</p>
+      <p>pocket money left over</p>
+      <p>card amount left</p>
+
       {/* <Navbar
         isParent={getUser?.data?.userQuery?.is_parent ?? false}
         userId={getUser?.data?.userQuery?.id}
         userName={getUser?.data?.userQuery?.name ?? ""}
       /> */}
-      <div>111</div>
     </div>
   );
 }
