3375f44afec1d562fe6813ec3b90da666807df56
ChristopherRdeKoker <christopherdekoker@gmail.com>
2024-08-29 22:46:09 +0200

    a

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -8,7 +8,7 @@ type NavbarProps = {
 };
 export function Navbar({ isParent = false, userId, userName }: NavbarProps) {
   return (
-    <div className="bg-slate-800 mx-auto p-2  rounded-sm">
+    <div className="bg-slate-800 rounded-sm">
       <p className="text-white">{userName && `Welcome back ${userName}`}</p>
       <NavButtons userId={userId} isParent={isParent} />
     </div>
