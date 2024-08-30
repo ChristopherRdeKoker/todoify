fd23b5f46ff70353bbcf714becfb7f930037f137
Chris <chris@owlbox.com>
2024-08-30 12:40:46 +0200

    1

--- database/seed/seed.sql
+++ database/seed/seed.sql
@@ -1,6 +1,6 @@
--- -- Insert 3 users into the account_user table
--- INSERT INTO account_user (id, name, password, username, is_parent)
--- VALUES 
---   (1,'Chris', '123456', 'chris', true),
---   (2,'Kelly', '123456', 'kelly',  false),
---   (3,'Troy', '6265','troy',  true);
+-- Insert 3 users into the account_user table
+INSERT INTO account_user (id, name, password, username, is_parent)
+VALUES 
+  (1,'Chris', '123456', 'chris', true),
+  (2,'Kelly', '123456', 'kelly',  true),
+  (3,'Troy', '6265','troy',  false);
