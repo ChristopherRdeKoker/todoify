dee79045d19e85ee4a259a8c8e6843a8678a2672
ChristopherRdeKoker <christopherdekoker@gmail.com>
2024-08-30 23:11:14 +0200

    13

--- src/app/[userId]/listTodo/page.tsx
+++ src/app/[userId]/listTodo/page.tsx
@@ -1,8 +1,5 @@
-import { Navbar } from "@/components/Navbar/Navbar";
-import { getUserQuery } from "../homepage/actions";
 import { Container } from "@/components/Container";
 import { getMyTodoList } from "./actions";
-import { TodoCard } from "./TodoCard";
 import { ListItem } from "./ListItem";
 
 type ListToDoParams = {
@@ -12,16 +9,13 @@ export default async function ListToDo({ params }: { params: ListToDoParams }) {
   const todoData = await getMyTodoList(+params?.userId);
 
   return (
-    <div className="">
+    <div className="p-2">
       <Container>
         <p className="font-bold underline">TO DO LIST:</p>
       </Container>
       <div className="flex grow">
         <div className="flex grow max-h-[calc(75vh)] overflow-y-auto gap-2 flex-col p-1">
-          {!!todoData?.data?.result &&
-            todoData?.data?.result?.map((i) => (
-              <ListItem input={i ?? []} key={i?.id} />
-            ))}
+          {!!todoData?.data?.result && todoData?.data?.result?.map((i) => <ListItem input={i ?? []} key={i?.id} />)}
         </div>
       </div>
     </div>
